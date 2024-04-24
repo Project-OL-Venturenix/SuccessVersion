f82f6dc36b4887ae140ea3f67eb804352289a58c
Oscar <lokongkitoscar@gmail.com>
2024-04-24 11:20:47 +0800

    npm run build

--- UserPage_Oscar/webpack.config.js
+++ UserPage_Oscar/webpack.config.js
@@ -34,7 +34,8 @@ const commonConfig = merge([
       ],
     },
     plugins: [
-      new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development') }),
+      // new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development') }),
+      new webpack.DefinePlugin({ 'process.env.NODE_ENV': process.env.NODE_ENV || 'development' }),
       new CleanWebpackPlugin([outputDirectory]),
       new HtmlWebpackPlugin({
         title: 'Online Code Editor',
@@ -89,65 +90,11 @@ module.exports = (env) => {
   console.log(env)
   if (env.production === true) {
     console.log('production')
-    return merge(commonConfig, productionConfig);
+    return merge(commonConfig, productionConfig, { mode: 'production' });
   }
 
   console.log('development')
-  const dev = merge(commonConfig, developmentConfig);
+  const dev = merge(commonConfig, developmentConfig, { mode: 'development' });
   console.log(dev);
   return dev;
 };
-
-/*
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-
-const outputDirectory = 'dist';
-
-module.exports = {
-  entry: './src/client/index.js',
-  output: {
-    path: path.join(__dirname, outputDirectory),
-    filename: 'bundle.js',
-    publicPath: '/',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 10000,
-        },
-      },
-    ],
-  },
-  devServer: {
-    port: 3000,
-    open: true,
-    proxy: {
-      '/api': 'http://localhost:8080',
-    },
-    historyApiFallback: true,
-  },
-  plugins: [
-    new CleanWebpackPlugin([outputDirectory]),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      favicon: './public/favicon.ico',
-    }),
-  ],
-};
-*/
